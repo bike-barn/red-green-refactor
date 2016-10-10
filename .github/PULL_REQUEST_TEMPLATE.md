23940772013cb3a69a06a624362df21ddb37e41d
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2016-10-09 21:51:04 -0700

    Create project meta files. Closes #2
    I've added issue and pull request templates, contributing guidelines, a
    blank features file, module scaffolding, requirements files, a tests
    module, and some setuptools files. I've omitted grammar diagrams from
    this commit because I don't know what those should look like yet.

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,9 @@
+
+### Description
+
+Write here
+
+### Check List
+
+- [ ] All test passed
+- [ ] Added test to ensure to fix/ensure properly.
---
+++ calc/__init__.py
@@ -0,0 +1,3 @@
+__author__ = 'Reilly Tucker Siemens, Alex LordThorsen'
+__email__ = 'reilly@tuckersiemens.com, alexlordthorsen@gmail.com'
+__version__ = '0.1.0'
