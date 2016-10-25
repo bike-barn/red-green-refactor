7d72895e37fd24b748c2051cc3e639f04d4d31e0
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2016-10-24 19:39:47 -0700

    Make token tests pass again

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -5,25 +5,29 @@ the interpreter doesn't have any data validation as a feature.
 """
 import pytest
 
-import pascal_interpreter as augur
+from calc import INTEGER, Token
+
 
 def test_no_defaults():
     # There's no valid defaults at the moment.
     with pytest.raises(TypeError):
-        augur.Token()
+        Token()
+
 
 def test_known_type():
     # There's no valid defaults at the moment.
-    token = augur.Token(type=augur.INTEGER, value=2)
+    token = Token(type=INTEGER, value=2)
     assert token.value == 2
-    assert token.type == augur.INTEGER
+    assert token.type == INTEGER
+
 
 def test_str_non_string_value():
-    token = augur.Token(type=augur.INTEGER, value=2)
+    token = Token(type=INTEGER, value=2)
     expected_result = "Token(type=INTEGER, value=2)"
     assert token.__str__() == expected_result
 
+
 def test_repr():
-    token = augur.Token(type=augur.INTEGER, value=2)
+    token = Token(type=INTEGER, value=2)
     expected_result = "Token(type=INTEGER, value=2)"
     assert token.__repr__() == expected_result
