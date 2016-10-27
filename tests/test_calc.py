13bb7288bf0256fbb95cb358967d9120469ecc37
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2016-10-26 22:56:40 -0700

    Improve documentation in calc tests. Remove extraneous tests

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -3,31 +3,32 @@ import pytest
 from calc import INTEGER, EOF, PLUS, Calc, CalcError
 
 
-def test_calc_blank_expression():
-    """This test just makes sure :class:`Interpreter` can be instantiated."""
+def test_calc_can_be_instantiated_with_blank_expression():
+    """
+    Test that an :class:`Calc` can be instantiated with a blank
+    expression.
+    """
     input_text = ""
     Calc(text=input_text)
 
 
-def test_raises_error_on_invalid_tokens():
+def test_calc_raises_error_on_invalid_tokens():
     """
     Test that invalid tokens cause a ``CalcError`` and that the exception stack
-    traace contains useful information.
+    trace contains useful information.
     """
-
-    input_text = "foo"
+    input_text = "lumberjack"  # Now with 100% more Monty Python references.
     calc = Calc(text=input_text)
     with pytest.raises(CalcError) as err:
         calc.parse()
     assert "Invalid token at position 0" in str(err.value)
 
 
-def test_raises_error_on_unexepected_syntax():
+def test_calc_raises_error_on_unexepected_syntax():
     """
     Test that unexpected syntax causes a ``CalcError`` and that the exception
     stack trace contains useful information.
     """
-
     input_text = "+"
     calc = Calc(text=input_text)
     with pytest.raises(CalcError) as err:
@@ -35,20 +36,21 @@ def test_raises_error_on_unexepected_syntax():
     assert "Expected INTEGER at position 1, found PLUS" in str(err.value)
 
 
-def test_invalid_string_errors():
-    input_text = "This is currently an error"
-    calc = Calc(text=input_text)
-    with pytest.raises(CalcError):
-        calc._next_token()
-
-
-def test_eof_token_at_end_of_line():
+def test_calc_finds_eof_token_at_end_of_line():
+    """
+    Test that, upon finding an end of line, a :class:`Calc` correctly tokenizes
+    an EOF :class:`Token`.
+    """
     input_text = ""
     calc = Calc(text=input_text)
     assert calc._next_token().type == EOF
 
 
-def test_eof_token_after_int():
+def test_calc_finds_eof_token_after_int():
+    """
+    Test that after consuming a solitary an INTEGER :class:`Token` a
+    :class:`Calc` will correctly tokenize an EOF :class:`Token`.
+    """
     input_text = "1"
     calc = Calc(text=input_text)
     token = calc._next_token()
@@ -57,7 +59,8 @@ def test_eof_token_after_int():
     assert calc._next_token().type == EOF
 
 
-def test_consume_valid_token():
+def test_calc_can_consume_valid_token():
+    """Test that a :class:`Calc` can consume a valid :class:`Token`."""
     input_text = "1+1"
     calc = Calc(text=input_text)
     # Note: Since _next_token advances position one cannot simply
@@ -68,15 +71,8 @@ def test_consume_valid_token():
     assert calc.current_token.type == PLUS
 
 
-def test_consume_invalid_token():
-    input_text = "+1"
-    calc = Calc(text=input_text)
-    calc.current_token = calc._next_token()
-    with pytest.raises(CalcError):
-        calc._consume_token(INTEGER)
-
-
-def test_parse_addition():
+def test_parse_supports_addition():
+    """Test that a :class:`Calc` can correctly parse the addition operation."""
     # Note: This function name was briefly duplicated and therefore didn't run.
     input_text = "1+1"
     calc = Calc(text=input_text)
@@ -84,13 +80,21 @@ def test_parse_addition():
 
 
 def test_parse_sets_eof():
+    """
+    Test that successfully parsing an arithmetic expression sets the
+    ``current_token`` attribute of a :class:`Calc` to EOF.
+    """
     input_text = "1+1"
     calc = Calc(text=input_text)
     calc.parse()
     assert calc.current_token.type == EOF
 
 
-def test_parse_invalid_addition():
+def test_parse_raises_error_on_invalid_expression():
+    """
+    Test that attempting to parse an invalid expression allows a ``CalcError``
+    to propagate correctly.
+    """
     input_text = "+1"
     calc = Calc(text=input_text)
     with pytest.raises(CalcError):
